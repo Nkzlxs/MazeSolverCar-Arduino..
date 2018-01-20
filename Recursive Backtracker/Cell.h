a19387b746a3b7d315dfcf37136ae5f6f243ba7c
Nkzlxs <35626992+Nkzlxs@users.noreply.github.com>
2018-01-20 20:24:26 +0800

    Update Cell.h

--- Recursive Backtracker/Cell.h
+++ Recursive Backtracker/Cell.h
@@ -15,7 +15,6 @@ public:
 
 	
 	std::vector<Line> m_sides; //The "sides" of this cell
-	short isCorner = 5;//redundant
 	
 	
 	//top left right btm
@@ -29,27 +28,9 @@ public:
 
 	Cell(float xpos, float ypos);
 	~Cell();
-	void drawRect(sf::Vector2f size) {
-		a.setPosition(sf::Vector2f(m_xpos, m_ypos));
-		a.setSize(size);
-	}
-	void drawRect(sf::Vector2f size,sf::Color c) {
-		a.setPosition(sf::Vector2f(m_xpos, m_ypos));
-		a.setFillColor(c);
-		a.setSize(size);
-	}
+	void drawRect(sf::Vector2f size);
+	void drawRect(sf::Vector2f size, sf::Color c);
 	void addNeighbours(bool t, bool l, bool r, bool b);
-
-	void display(sf::RenderWindow &window1) {
-		for (int i = 0;i < 4; i++) {
-			if (!m_status[i])
-				m_sides[i] = null;
-		}
-		for (Line side : m_sides) {
-			if (side != null)
-				window1.draw(side.m_self);
-		}
-		window1.draw(a);
-	}
+	void display(sf::RenderWindow &window1);
 };
 
